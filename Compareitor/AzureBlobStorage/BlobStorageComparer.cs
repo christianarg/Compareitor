4a72110bf6bda832e8ac67e81103421af6ca8511
Christian Rodriguez <coso@coso.com>
2018-03-31 21:33:53 +0200

    para blob. falta probar

---
+++ Compareitor/AzureBlobStorage/BlobStorageComparer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Compareitor.CommonModel;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace Compareitor.AzureBlobStorage
+{
+    public class BlobStorageComparer : PerformanceComparerBase
+    {
+        CloudStorageAccount storageAccount;
+        CloudBlobClient blobClient;
+        CloudBlobContainer container;
+
+        public override void Setup()
+        {
+            storageAccount = CloudStorageAccount.Parse("blobConnection");
+
+            blobClient = storageAccount.CreateCloudBlobClient();
+
+            container = blobClient.GetContainerReference("invoices");
+            container.CreateIfNotExists();
+        }
+
+        protected override void ExecuteWrite(List<Invoice> invoices)
+        {
+            foreach (var invoice in invoices)
+            {
+                var blob = container.GetBlockBlobReference(invoice.Id.ToString());
+                blob.UploadText(JsonConvert.SerializeObject(invoice));
+            }
+        }
+
+        protected override void ExecuteRead(List<Invoice> invoices)
+        {
+            foreach (var invoice in invoices)
+            {
+                var blob = container.GetBlockBlobReference(invoice.Id.ToString());
+                JsonConvert.DeserializeObject<Invoice>(blob.DownloadText());
+            }
+        }
+    }
+}
